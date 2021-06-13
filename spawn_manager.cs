2ed081d2a6fb8ca1c021220a3b00ee25fb5e2eeb
harsh2332 <85254560+harsh2332@users.noreply.github.com>
2021-06-13 14:28:32 +0530

    Add files via upload

---
+++ spawn_manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_manager : MonoBehaviour
+{
+
+[SerializeField]
+    private GameObject _enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine(spawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+IEnumerator spawnRoutine()
+{
+  while(true)
+  {
+      Vector3 posToSpawn = new Vector3(Random.Range(5f, -5f ), 4, 0);
+     Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+     yield return new WaitForSeconds(1.0f);
+  }
+
+
+}
+
+
+}
