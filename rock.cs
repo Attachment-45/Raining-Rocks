bbcd2f536f26c15ead5cca0dfc0244379eea49f5
harsh2332 <85254560+harsh2332@users.noreply.github.com>
2021-06-13 14:33:57 +0530

    Add files via upload

---
+++ rock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour
+{   
+   [SerializeField]
+  private GameObject gameObject;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * 2.3f * Time.deltaTime);
+
+        if (transform.position.y <= -3.25f) 
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
